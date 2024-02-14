379de41ecbb5881d095923569fdf9cbcbf4e7c2c
Luca Camphuisen <luca.camphuisen@adesso.nl>
2024-02-14 14:42:51 +0100

    Make code more kotlin-ish

--- greed-kotlin/src/main/kotlin/Greed.kt
+++ greed-kotlin/src/main/kotlin/Greed.kt
@@ -4,10 +4,9 @@ package nl.adesso.kgreed
  * Created on 07/02/2024
  */
 class Greed {
+
     companion object {
-        fun countOccurrences(input: IntArray, num: Int): Int {
-            return input.count { it == num }
-        }
+        private fun IntArray.occurrences(value: Int) = count { it == value }
         fun playGame(scoreGoal: Int, inputFunction: (play: Int) -> String): Int {
             var currentPlayer = 0;
             val scores = intArrayOf(0,0)
@@ -26,29 +25,19 @@ class Greed {
             }
         }
 
-        fun score(input: IntArray): Int {
-            if (input.distinct().size == 6) {
-                return 1200
-            }
-            if (input.distinct().size == 3 && input.all { countOccurrences(input, it) == 2 }) {
-                return 800
-            }
-            var totalScore = 0
-            numberLoop@ for (number in input.distinct()) {
-                val occurrences = countOccurrences(input, number)
-                if ((number == 1 || number == 5) && occurrences < 3) {
-                    totalScore += occurrences * (if(number == 1) 100 else 50)
-                } else {
-                    if (occurrences < 3) {
-                        continue@numberLoop
-                    }
-                    var tripleScore = if (number == 1) 1000 else (number * 100)
-                    val multiplier = 1 shl (occurrences - 3)
-                    tripleScore *= multiplier
-                    totalScore += tripleScore
+        fun score(input: IntArray) = when {
+            (input.distinct().size == 6) -> 1200
+            (input.distinct().size == 3 && input.all { input.occurrences(it) == 2 }) -> 800
+            else -> input.distinct().map { it to input.occurrences(it) }.sumOf { (number, occurrences) ->
+                when {
+                    // first = the number
+                    // second = the occurrences of number
+                    (number in setOf(1, 5) && occurrences < 3) -> occurrences * if (number == 1) 100 else 50
+                    occurrences >= 3 -> (if (number == 1) 1000 else (number * 100)) * (1 shl (occurrences - 3))
+                    else -> 0
                 }
             }
-            return totalScore
+
         }
     }
 }
--- greed-kotlin/src/main/kotlin/Main.kt
+++ greed-kotlin/src/main/kotlin/Main.kt
@@ -1,18 +1,13 @@
 package nl.adesso.kgreed
 
+import java.util.*
+
 /**
  * Created on 07/02/2024
  *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val scan = Scanner(System.`in`)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    Greed.playGame(4000) { scan.nextLine() }
 }
